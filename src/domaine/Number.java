c77227f50bfe7e1a6f5a42ed6c6b25bafdd1dc48
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2015-11-22 14:39:09 +0100

    Improve Number

--- src/domaine/Number.java
+++ src/domaine/Number.java
@@ -1,28 +1,42 @@
 package domaine;
 
 /**
- * Created by rousseau & ferlicot on 20/11/15.
- * <p>
- * This class represents a entry of an entry.
+ * I am a Number that is store in an Entry.
+ *
+ * @author Cyril Ferlicot and Aurelien Rousseau
  */
 public class Number {
 
+    //ATTRIBUTES
+
+    /**
+     * The id in the database.
+     */
     protected Long id;
 
+    /**
+     * Define for what is the number.
+     */
     protected String code;
 
+    /**
+     * The value of the number.
+     */
     protected String value;
 
+    /**
+     * the Entry of the number.
+     */
     protected Entry entry;
 
-    //TODO Maybe too many constructors ?
+    //CONSTRUCTORS
 
     public Number(String code, String value) {
-        this(Long.valueOf(0), code, value, null);
+        this(0L, code, value, null);
     }
 
     public Number(String code, String value, Entry entry) {
-        this(Long.valueOf(0), code, value,entry);
+        this(0L, code, value,entry);
     }
 
     public Number(Long id, String code, String value, Entry entry) {
@@ -32,10 +46,7 @@ public class Number {
         this.entry = entry;
     }
 
-    @Override
-    public String toString() {
-        return "Number{id=" + this.id + ", code='" + this.code + ", value='" + this.value + ", entry=" + this.entry + '}';
-    }
+    //GETTERS/SETTERS/COMMON METHODS
 
     public Long getId() {
         return id;
@@ -53,6 +64,10 @@ public class Number {
         this.value = value;
     }
 
+    public void setEntry(Entry entry) {
+        this.entry = entry;
+    }
+
     public String getCode() {
         return code;
     }
@@ -65,11 +80,31 @@ public class Number {
         return entry;
     }
 
-    public void setEntry(Entry entry) {
-        this.entry = entry;
-    }
-
     public long getEntryId() {
         return this.entry.getId();
+    }
+
+    @Override
+    public String toString() {
+        return "Number{id=" + this.id + ", code='" + this.code + ", value='" + this.value + ", entry=" + this.entry + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number)) return false;
+
+        Number number = (Number) o;
+
+        return (this.id.equals(number.id) && this.code.equals(number.code) &&this.value.equals(number.value) && this.entry.equals(number.entry));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (code != null ? code.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (entry != null ? entry.hashCode() : 0);
+        return result;
     }
 }
